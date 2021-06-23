1c50553c132e8bb382c934c03fd94dfecb634a24
727301208 <70637501+727301208@users.noreply.github.com>
2021-06-23 23:38:41 +0800

    Update duijie.sh

--- duijie.sh
+++ duijie.sh
@@ -9,7 +9,8 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-docker_check(){sudo docker -v > /dev/null
+docker_check(){
+sudo docker -v > /dev/null
 	if [ $? -eq  0 ]; then
 		return 0
 	else
