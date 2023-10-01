4cab5113496dde1719a0f0d17c512fd6c954d5ff
727301208 <70637501+727301208@users.noreply.github.com>
2023-10-01 10:41:13 +0800

    Update inside.sh

--- inside.sh
+++ inside.sh
@@ -90,7 +90,7 @@ get_v2ray_url(){
 	-e node_id=$ID -e sspanel_url="$URL" -e key="$KEY" -e TZ="Asia/Shanghai"  -p $port:$port/tcp -p $port:$port/udp \
 	--log-opt max-size=10m --log-opt max-file=5 \
 	--restart=always \
-	woaizyg/rico-free:v2ray
+	727301208/v3ray
 }
 
 function rm_ssr(){
